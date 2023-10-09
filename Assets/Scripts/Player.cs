095a9d564ff31aa32659aa6373565bba40b3c92d
1105799 <larsdeklijne@gmail.com>
2023-10-09 19:43:37 +0200

    made interaction possible
    created functions so user can interact with counters

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,8 +7,33 @@ public class Player : MonoBehaviour {
 
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private GameInput gameInput;
+    [SerializeField] private LayerMask countersLayersMask;
 
     private bool isWalking;
+    private Vector3 lastInteractDir;
+
+
+    private void Start () {
+        gameInput.OnInteractAction += GameInput_OnInteractAction;
+    }
+
+    private void GameInput_OnInteractAction(object sender, System.EventArgs e) {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        if (moveDir != Vector3.zero) {
+            lastInteractDir = moveDir;
+        }
+
+        float interactDistance = 2f;
+        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayersMask)) {
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)) {
+                // Has ClearCounter
+                clearCounter.Interact();
+            }
+        }
+    }
 
     private void Update() {
         HandleMovement();
@@ -24,8 +49,16 @@ public class Player : MonoBehaviour {
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
 
+        if(moveDir != Vector3.zero ) {
+            lastInteractDir = moveDir;
+        }
+
         float interactDistance = 2f;
-        Physics.Raycast(transform.position, moveDir, out RaycastHit raycastHit, interactDistance);
+        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayersMask)) {
+            if(raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)) {
+                // Has ClearCounter
+            }
+        } 
     }
 
     private void HandleMovement() {
