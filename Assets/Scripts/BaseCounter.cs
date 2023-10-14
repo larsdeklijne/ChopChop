a579dd8994e4071e5f19ae53f41803ee46a09d59
1105799 <larsdeklijne@gmail.com>
2023-10-14 10:07:19 +0200

    ClearCounter selector works visually
    Bugg fixed: ClearCounter selector now works visually again

--- Assets/Scripts/BaseCounter.cs
+++ Assets/Scripts/BaseCounter.cs
@@ -1,2 +1,11 @@
-﻿public class BaseCounter {
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseCounter : MonoBehaviour {
+ 
+    public virtual void Interact(Player player) {
+        Debug.LogError("BaseCounter.Interact()");
+    }
+
 }
--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClearCounter : MonoBehaviour, IKitchenObjectParent {
+public class ClearCounter : BaseCounter, IKitchenObjectParent {
 
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
     [SerializeField] private Transform counterTopPoint;
 
     private KitchenObject kitchenObject;
 
-    public void Interact(Player player) {
+    public override void Interact(Player player) {
 
         if (kitchenObject == null) {
             Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
