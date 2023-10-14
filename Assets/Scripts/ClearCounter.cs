51ebcdbced541705af62d6333fec453f632a746d
1105799 <larsdeklijne@gmail.com>
2023-10-14 13:37:35 +0200

    clearCounter kitchenObject pickup/ drop works
    clearCounter kitchenObject pickup/ drop works

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -9,6 +9,26 @@ public class ClearCounter : BaseCounter {
     public override void Interact(Player player) {
 
         // only pickup and drop items
+        if (!HasKitchenObject()) {
+
+            // there is no kitchenObject
+            if (player.HasKitchenObject()) {
+                // player is carrying something
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            } else {
+                // Player has carrying anything
+            }
+
+        } else {
+            // there is a kitchenObject
+            if (player.HasKitchenObject()) {
+                // player is carrying something
+            } else {
+                // Player is not carrying anything
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
+
     }
 
 }
