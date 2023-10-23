977529a1408bcfe8836225adaff45a0605590f1b
1105799 <larsdeklijne@gmail.com>
2023-10-23 08:13:57 +0200

    plateCounter fully works
    bugg fixed and plate timer now works keeps on spawning 4 at a time

---
+++ Assets/Scripts/Counters/PlatesCounter.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatesCounter : BaseCounter {
+
+    public event EventHandler OnPlateSpawned;
+    public event EventHandler OnPlateRemoved;
+
+
+    [SerializeField] private KitchenObjectSO plateKitchenObjectSO;
+
+    private float spawnPlateTimer;
+    private float spawnPlateTimerMax = 4f;
+    private int platesSpawnedAmount;
+    private int plateSpawnedAmountMax = 4;
+
+    private void Update() {
+        spawnPlateTimer += Time.deltaTime;
+        if (spawnPlateTimer > spawnPlateTimerMax) {
+            spawnPlateTimer = 0f;
+
+            if (platesSpawnedAmount < plateSpawnedAmountMax) {
+                Debug.Log("timer over spawn plate");
+
+                platesSpawnedAmount++;
+
+                OnPlateSpawned?.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+
+    public override void Interact(Player player) {
+        if (!player.HasKitchenObject()) {
+            // player is empty handed
+            if (platesSpawnedAmount > 0) {
+                // there is at least 1 plate here
+                platesSpawnedAmount--;
+
+                KitchenObject.SpawnKitchenObject(plateKitchenObjectSO, player);
+
+                OnPlateRemoved?.Invoke(this, EventArgs.Empty);
+            }
+
+        }
+    }
+
+
+
+}
