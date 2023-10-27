990b7c9885a2430f51753a1f36b6b6e2d3a5f047
1105799 <larsdeklijne@gmail.com>
2023-10-27 08:44:44 +0200

    music is now played, and fail/ succes recipe fx
    music is now played, and fail/ succes recipe fx

--- Assets/Scripts/DeliveryManager.cs
+++ Assets/Scripts/DeliveryManager.cs
@@ -9,6 +9,8 @@ public class DeliveryManager : MonoBehaviour {
 
     public event EventHandler OnRecipeSpawned;
     public event EventHandler OnRecipeCompleted;
+    public event EventHandler OnRecipeSuccess;
+    public event EventHandler OnRecipeFailed;
 
     public static DeliveryManager Instance {  get; private set; }
 
@@ -68,6 +70,7 @@ public class DeliveryManager : MonoBehaviour {
                     waitingRecipeSOList.RemoveAt(i);
 
                     OnRecipeCompleted?.Invoke(this, EventArgs.Empty);
+                    OnRecipeSuccess?.Invoke(this, EventArgs.Empty);
                     return;
                 }
             }
@@ -75,6 +78,7 @@ public class DeliveryManager : MonoBehaviour {
 
         // No matches found!
         // Player did not deliver a correct recipe
+        OnRecipeFailed?.Invoke(this, EventArgs.Empty);
     }
 
     public List<RecipeSO> GetWaitingRecipeSOList() {
